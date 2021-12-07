fe96b3cd486ccd9d76263bea88b78e57ed6e7a05
Adphi <philippe.adrien.nousse@gmail.com>
2021-12-07 15:04:52 +0100

    logger: move to interface

--- device/logger.go
+++ device/logger.go
@@ -15,9 +15,25 @@ import (
 // They must be safe for concurrent use.
 // They do not require a trailing newline in the format.
 // If nil, that level of logging will be silent.
-type Logger struct {
-	Verbosef func(format string, args ...any)
-	Errorf   func(format string, args ...any)
+type Logger interface {
+	Verbosef(format string, args ...any)
+	Errorf(format string, args ...any)
+}
+
+type logger struct {
+	verbosef func(format string, args ...any)
+	errorf   func(format string, args ...any)
+}
+
+func (l *logger) Verbosef(format string, args ...any) {
+	if l.verbosef != nil {
+		l.verbosef(format, args...)
+	}
+}
+func (l *logger) Errorf(format string, args ...any) {
+	if l.errorf != nil {
+		l.errorf(format, args...)
+	}
 }
 
 // Log levels for use with NewLogger.
@@ -33,16 +49,16 @@ func DiscardLogf(format string, args ...any) {}
 // NewLogger constructs a Logger that writes to stdout.
 // It logs at the specified log level and above.
 // It decorates log lines with the log level, date, time, and prepend.
-func NewLogger(level int, prepend string) *Logger {
-	logger := &Logger{DiscardLogf, DiscardLogf}
+func NewLogger(level int, prepend string) Logger {
+	logger := &logger{}
 	logf := func(prefix string) func(string, ...any) {
 		return log.New(os.Stdout, prefix+": "+prepend, log.Ldate|log.Ltime).Printf
 	}
 	if level >= LogLevelVerbose {
-		logger.Verbosef = logf("DEBUG")
+		logger.verbosef = logf("DEBUG")
 	}
 	if level >= LogLevelError {
-		logger.Errorf = logf("ERROR")
+		logger.errorf = logf("ERROR")
 	}
 	return logger
 }
